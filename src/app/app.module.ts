82aee9111871a660473ec4fb32ada1815a3f9f2f
Cristhian Eduardo Castillo Erazo <cece9707@gmail.com>
2018-10-01 16:00:10 -0500

    Agregar propiedades Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+/**
+ * Componentes
+ */
+import { LoginPage } from '../pages/login/login';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -12,16 +16,24 @@ import { FacturasPage } from '../pages/facturas/facturas';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
+/**
+ * Servicios
+ */
+import { HttpClientModule } from "@angular/common/http";
+import { LoginProvider } from '../providers/login/login';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,FacturasPage
+    TabsPage,LoginPage,FacturasPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -30,12 +42,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,FacturasPage
+    TabsPage,LoginPage,FacturasPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LoginProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,38 @@ import { FacturasPage} from '../facturas/facturas';
 })
 export class HomePage {
 
+  empacadores = [
+    {
+      rack: 'Rack 1',
+      nombre: 'Jose Beltran',
+      facturas: 4
+    },
+    {
+      rack: 'Rack 2',
+      nombre: 'Carlos Rodriguez',
+      facturas: 5
+    },
+    {
+      rack: 'Rack 3',
+      nombre: 'Alberto Casas',
+      facturas: 10
+    },
+    {
+      rack: 'Rack 4',
+      nombre: 'Mauricio Martinez',
+      facturas: 6
+    },
+    {
+      rack: 'Rack 5',
+      nombre: 'Camilo Ballesteros',
+      facturas: 5
+    },
+    {
+      rack: 'Rack 6',
+      nombre: 'Federico Pulido',
+      facturas: 7
+    }
+  ]
   constructor(public navCtrl: NavController) {
 
   }
